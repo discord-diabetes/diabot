fde4d291a037f87e6f02aad4f79c62334616a898
Adi Barilan <adibarilan@gmail.com>
2018-10-02 22:03:38 +0300

    [WIP] #1 Add example command

--- src/main/java/com/dongtronic/diabot/Main.java
+++ src/main/java/com/dongtronic/diabot/Main.java
@@ -2,27 +2,72 @@ package com.dongtronic.diabot;
 
 import javax.security.auth.login.LoginException;
 
+import com.dongtronic.diabot.commands.PingCommand;
 import com.dongtronic.diabot.listener.RoleListener;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDABuilder;
+import com.jagrosh.jdautilities.command.CommandClientBuilder;
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import com.jagrosh.jdautilities.examples.command.AboutCommand;
+import net.dv8tion.jda.core.*;
+import net.dv8tion.jda.core.entities.Game;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.awt.*;
 
 public class Main {
 
   private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws LoginException {
 
     //TODO: configure logging levels
 
     String token = System.getenv("discord-rolebot-token");
 
-    try {
-      JDA jda = new JDABuilder(token).addEventListener(new RoleListener()).build().awaitReady();
-    } catch (LoginException | InterruptedException ex) {
-      logger.error(ex.getMessage());
-    }
+    // define an eventwaiter, dont forget to add this to the JDABuilder!
+    EventWaiter waiter = new EventWaiter();
+
+    // define a command client
+    CommandClientBuilder client = new CommandClientBuilder();
+
+    // The default is "Type !!help" (or whatver prefix you set)
+    client.useDefaultGame();
+
+    // sets emojis used throughout the bot on successes, warnings, and failures
+    client.setEmojis("\uD83D\uDE03", "\uD83D\uDE2E", "\uD83D\uDE26");
+
+    // sets the bot prefix
+    client.setPrefix("69");
+
+    client.setOwnerId("125616270254014464");
+
+    // adds commands
+    client.addCommands(
+        // command to show information about the bot
+        new AboutCommand(Color.BLUE, "an example bot",
+            new String[]{"Cool commands","Nice examples","Lots of fun!"},
+            new Permission[]{Permission.ADMINISTRATOR}),
+
+        // command to check bot latency
+        new PingCommand());
+
+
+    // start getting a bot account set up
+    new JDABuilder(AccountType.BOT)
+        // set the token
+        .setToken(token)
+
+        // set the game for when the bot is loading
+        .setStatus(OnlineStatus.DO_NOT_DISTURB)
+        .setGame(Game.playing("loading..."))
+
+        // add the listeners
+        .addEventListener(waiter)
+        .addEventListener(client.build())
+
+        // start it up!
+        .build();
+
 
   }
 
---
+++ src/main/java/com/dongtronic/diabot/commands/PingCommand.java
@@ -0,0 +1,30 @@
+package com.dongtronic.diabot.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import com.jagrosh.jdautilities.doc.standard.CommandInfo;
+
+import java.time.temporal.ChronoUnit;
+
+@CommandInfo(
+    name = {"Ping", "Pong"},
+    description = "Checks the bot's latency"
+)
+
+public class PingCommand extends Command {
+
+  public PingCommand() {
+    this.name = "ping";
+    this.help = "checks the bot's latency";
+    this.guildOnly = false;
+    this.aliases = new String[]{"pong"};
+  }
+
+  @Override
+  protected void execute(CommandEvent event) {
+    event.reply("Ping: ...", m -> {
+      long ping = event.getMessage().getCreationTime().until(m.getCreationTime(), ChronoUnit.MILLIS);
+      m.editMessage("Ping: " + ping + "ms | Websocket: " + event.getJDA().getPing() + "ms").queue();
+    });
+  }
+}
