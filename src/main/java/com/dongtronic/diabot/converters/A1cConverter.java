9672a22caeb24429adce47fe59107d1fedf395f7
Adi Barilan <adibarilan@gmail.com>
2018-10-03 19:24:26 +0300

    [WIP] #3 Add more files

--- src/main/java/com/dongtronic/diabot/converters/A1cConverter.java
+++ src/main/java/com/dongtronic/diabot/converters/A1cConverter.java
@@ -1,7 +1,52 @@
 package com.dongtronic.diabot.converters;
 
+import com.dongtronic.diabot.data.A1cDTO;
+import com.dongtronic.diabot.data.ConversionDTO;
+import com.dongtronic.diabot.exceptions.UnknownUnitException;
+
 /**
  * A1c conversion logic
  */
 public class A1cConverter{
+
+  private static A1cDTO convert(double originalValue) {
+    A1cDTO result = null;
+    try {
+      if (originalValue < 25) {
+        //Convert to mg/dL
+        result = convert(originalValue, "mmol");
+      } else if (originalValue > 50) {
+        //Convert to mmol/L
+        result = convert(originalValue, "mgdl");
+      } else {
+        result = convertAmbiguous(originalValue);
+      }
+    } catch (UnknownUnitException ex) {
+      // Ignored on purpose
+    }
+
+    return result;
+  }
+
+  private static A1cDTO convert(double originalValue, String unit) throws UnknownUnitException {
+
+    if (unit.toUpperCase().contains("MMOL")) { //Convert to mg/dL
+      double result = originalValue * 18.016;
+      return new A1cDTO(originalValue, result, GlucoseUnit.MMOL);
+    } else if (unit.toUpperCase().contains("MG")) { //Convert to mmol/L
+      double result = originalValue / 1.8016;
+      return new A1cDTO(originalValue, result, GlucoseUnit.MGDL);
+    } else {
+      throw new UnknownUnitException();
+    }
+  }
+
+  private static A1cDTO convertAmbiguous(double originalValue) {
+
+    double toMgdl = originalValue * 18.016;
+    double toMmol = originalValue / 18.016;
+
+    return new A1cDTO(originalValue, toMmol, toMgdl);
+
+  }
 }
