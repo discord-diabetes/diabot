9037d5d561c77e70ae03301ceeeb97709b052c73
Cas Eliëns <cas.eliens@isaac.nl>
2019-04-24 08:25:41 +0200

    Fix incorrect GitHub link

--- src/main/java/com/dongtronic/diabot/commands/misc/GithubCommand.kt
+++ src/main/java/com/dongtronic/diabot/commands/misc/GithubCommand.kt
@@ -20,7 +20,7 @@ class GithubCommand(category: Command.Category) : DiabotCommand(category, null) 
 
         builder.setDescription("https://github.com/reddit-diabetes/diabot-discord")
 
-        builder.addField("Issues", "https://github.com/reddit/diabetes/diabot/discord/issues", true)
+        builder.addField("Issues", "https://github.com/reddit-diabetes/diabot-discord/issues", true)
         builder.addField("Bug report", "https://github.com/reddit-diabetes/diabot-discord/issues/new?template=bug_report.md", true)
         builder.addField("Feature request", "https://github.com/reddit-diabetes/diabot-discord/issues/new?template=feature_request.md", true)
 
