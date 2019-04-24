1293335c69a1d23dfddc5767ffd8c29144306675
Cas Eliëns <cas.eliens@isaac.nl>
2019-04-24 08:27:44 +0200

    Fix issue where Diabot converts numbers inside URLs
    Now requires a space before the inline underscore.
    Close #55

--- src/main/java/com/dongtronic/diabot/util/Patterns.kt
+++ src/main/java/com/dongtronic/diabot/util/Patterns.kt
@@ -3,7 +3,7 @@ package com.dongtronic.diabot.util
 import java.util.regex.Pattern
 
 object Patterns {
-    val inlineBgPattern = Pattern.compile("^.*_([0-9]{1,3}\\.?[0-9]?)_.*$")!!
+    val inlineBgPattern = Pattern.compile("^.*\\s_([0-9]{1,3}\\.?[0-9]?)_.*$")!!
     val separateBgPattern = Pattern.compile("^([0-9]{1,3}\\.?[0-9]?)$")!!
     val unitBgPattern = Pattern.compile("^((.*\\s-?)|(-?))?([0-9.]+)\\s?((mmol)|(mg)).*$")!!
     val feelPattern = Pattern.compile(".*feel it.*")!!
