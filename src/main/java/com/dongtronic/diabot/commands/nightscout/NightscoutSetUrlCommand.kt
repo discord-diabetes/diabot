01128d054db3c7644f027303d6f86730283abbe7
p5nbTgip0r <cb3f02c5@opayq.com>
2019-07-31 20:38:29 -0700

    Handle insufficient permissions properly while setting NS url

--- src/main/java/com/dongtronic/diabot/commands/nightscout/NightscoutSetUrlCommand.kt
+++ src/main/java/com/dongtronic/diabot/commands/nightscout/NightscoutSetUrlCommand.kt
@@ -5,6 +5,7 @@ import com.dongtronic.diabot.data.NightscoutDAO
 import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
 import net.dv8tion.jda.core.entities.User
+import net.dv8tion.jda.core.exceptions.InsufficientPermissionException
 import org.slf4j.LoggerFactory
 
 class NightscoutSetUrlCommand(category: Command.Category, parent: Command?) : DiabotCommand(category, parent) {
@@ -35,8 +36,12 @@ class NightscoutSetUrlCommand(category: Command.Category, parent: Command?) : Di
             event.replyError(ex.message)
         }
 
-        event.message.delete().reason("privacy").queue()
-        event.reply("Set Nightscout URL for ${event.author.name}")
+        try {
+            event.reply("Set Nightscout URL for ${event.author.name}")
+            event.message.delete().reason("privacy").queue()
+        } catch (ex: InsufficientPermissionException) {
+            event.replyError("Could not remove command message due to missing `manage messages` permission. Please remove the message yourself to protect your privacy.")
+        }
     }
 
     private fun validateNightscoutUrl(url: String): String {
