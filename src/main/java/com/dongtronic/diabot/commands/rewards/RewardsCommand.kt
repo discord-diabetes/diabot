bda91274f5782047567d3cd6909dbae9e69e39b4
p5nbTgip0r <cb3f02c5@opayq.com>
2019-10-22 07:16:31 -0700

    Convert rewards command into JDA subcommands

--- src/main/java/com/dongtronic/diabot/commands/rewards/RewardsCommand.kt
+++ src/main/java/com/dongtronic/diabot/commands/rewards/RewardsCommand.kt
@@ -1,8 +1,6 @@
 package com.dongtronic.diabot.commands.rewards
 
 import com.dongtronic.diabot.commands.DiabotCommand
-import com.dongtronic.diabot.data.RewardDAO
-import com.dongtronic.diabot.util.NicknameUtils
 import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
 import org.slf4j.LoggerFactory
@@ -16,50 +14,15 @@ class RewardsCommand(category: Command.Category) : DiabotCommand(category, null)
         this.help = "Opt in or out of automatic role rewards"
         this.guildOnly = true
         this.aliases = arrayOf("reward", "r")
-        this.examples = arrayOf("diabot rewards optout", "diabot rewards optin")
+        this.children = arrayOf(
+                RewardsOptInCommand(category, this),
+                RewardsOptOutCommand(category, this)
+        )
     }
 
     override fun execute(event: CommandEvent) {
-        val args = event.args.split("\\s+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        val subcommands = children.joinToString(", ") { it.name }
 
-        if (args.isEmpty()) {
-            throw IllegalArgumentException("must include operation")
-        }
-
-        val command = args[0].toUpperCase()
-
-        try {
-            when (command) {
-                "OPTIN", "OI", "I" -> optIn(event)
-                "OPTOUT", "OU", "O" -> optOut(event)
-                else -> {
-                    throw IllegalArgumentException("unknown command $command")
-                }
-            }
-        } catch (ex: IllegalArgumentException) {
-            event.replyError(ex.message)
-        }
-    }
-
-    private fun optIn(event: CommandEvent) {
-        val guildId = event.guild.id
-
-        val user = event.author
-
-        RewardDAO.getInstance().optIn(guildId, user.id)
-
-        logger.info("User ${user.name}#${user.discriminator} (${user.id}) opted in to rewards")
-        event.reply("User ${NicknameUtils.determineDisplayName(event, user)} opted in to rewards")
-    }
-
-    private fun optOut(event: CommandEvent) {
-        val guildId = event.guild.id
-
-        val user = event.author
-
-        RewardDAO.getInstance().optOut(guildId, user.id)
-
-        logger.info("User ${user.name}#${user.discriminator} (${user.id}) opted out of rewards")
-        event.reply("User ${NicknameUtils.determineDisplayName(event, user)} opted out of rewards")
+        event.replyError("Valid sub-commands are: $subcommands")
     }
 }
