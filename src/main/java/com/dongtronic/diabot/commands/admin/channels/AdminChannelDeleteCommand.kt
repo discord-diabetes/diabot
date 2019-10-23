a1bc9adde3cb5ea6a75ff963fe6a7f1b6a74b7c8
p5nbTgip0r <cb3f02c5@opayq.com>
2019-10-22 17:52:42 -0700

    Handle exceptions in admin channels subcommands

--- src/main/java/com/dongtronic/diabot/commands/admin/channels/AdminChannelDeleteCommand.kt
+++ src/main/java/com/dongtronic/diabot/commands/admin/channels/AdminChannelDeleteCommand.kt
@@ -20,21 +20,25 @@ class AdminChannelDeleteCommand(category: Category, parent: Command?) : DiabotCo
     override fun execute(event: CommandEvent) {
         val args = event.args.split("\\s+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
 
-        if (args.size != 1) {
-            throw IllegalArgumentException("Channel ID is required")
+        try {
+            if (args.size != 1) {
+                throw IllegalArgumentException("Channel ID is required")
+            }
+
+            if (!StringUtils.isNumeric(args[0])) {
+                throw IllegalArgumentException("Channel ID must be numeric")
+            }
+
+            val channelId = args[0]
+
+            val channel = event.jda.getTextChannelById(channelId)
+                    ?: throw IllegalArgumentException("Channel `$channelId` does not exist")
+
+            AdminDAO.getInstance().removeAdminChannel(event.guild.id, channelId)
+
+            event.reply("Removed admin channel ${channel.name} (`$channelId`)")
+        } catch (ex: IllegalArgumentException) {
+            event.replyError(ex.message)
         }
-
-        if (!StringUtils.isNumeric(args[0])) {
-            throw IllegalArgumentException("Channel ID must be numeric")
-        }
-
-        val channelId = args[0]
-
-        val channel = event.jda.getTextChannelById(channelId)
-                ?: throw IllegalArgumentException("Channel `$channelId` does not exist")
-
-        AdminDAO.getInstance().removeAdminChannel(event.guild.id, channelId)
-
-        event.reply("Removed admin channel ${channel.name} (`$channelId`)")
     }
 }
