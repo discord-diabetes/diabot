964514d0fb701cf7e39ed40d364bee6dee39a77f
p5nbTgip0r <cb3f02c5@opayq.com>
2020-07-28 23:24:47 -0700

    Fix duplicate quotes when two users react to the same message

--- src/main/java/com/dongtronic/diabot/platforms/discord/listeners/QuoteListener.kt
+++ src/main/java/com/dongtronic/diabot/platforms/discord/listeners/QuoteListener.kt
@@ -2,9 +2,14 @@ package com.dongtronic.diabot.platforms.discord.listeners
 
 import com.dongtronic.diabot.data.QuoteDAO
 import com.dongtronic.diabot.data.QuoteDTO
+import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import org.litote.kmongo.eq
 import org.slf4j.LoggerFactory
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+import java.util.function.Consumer
 
 class QuoteListener : ListenerAdapter() {
     // https://emojiguide.org/speech-balloon
@@ -19,7 +24,13 @@ class QuoteListener : ListenerAdapter() {
         val author = event.member
         val guild = event.guild
 
-        event.channel.retrieveMessageById(event.messageId).queue { message ->
+        val messageRetrieval = Mono.defer {
+            // defer to prevent retrieving message until subscribe
+            event.channel.retrieveMessageById(event.messageId)
+                .submit().toMono()
+        }
+
+        val quoteMessage = Consumer<Message> { message ->
             QuoteDAO.getInstance().addQuote(QuoteDTO(
                     guildId = guild.idLong,
                     channelId = event.channel.idLong,
@@ -34,5 +45,16 @@ class QuoteListener : ListenerAdapter() {
                 event.channel.sendMessage("Could not create quote for message: ${message.id}").queue()
             })
         }
+
+        QuoteDAO.getInstance()
+                // search for any quotes with this message ID
+                .getQuotes(guild.idLong, QuoteDTO::messageId eq event.messageIdLong)
+                .toMono()
+                .subscribe({/*ignored*/}, {
+                    if (it is NoSuchElementException) {
+                        // if there is no quotes then proceed
+                        messageRetrieval.subscribe(quoteMessage)
+                    }
+                })
     }
 }
