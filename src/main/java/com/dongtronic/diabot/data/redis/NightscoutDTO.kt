a39b2716eb5088b6e3cd2f1ad5b6f6790a619823
p5nbTgip0r <cb3f02c5@opayq.com>
2020-08-01 02:57:20 -0700

    Convert `NightscoutDTO` to data class

--- src/main/java/com/dongtronic/diabot/data/redis/NightscoutDTO.kt
+++ src/main/java/com/dongtronic/diabot/data/redis/NightscoutDTO.kt
@@ -3,24 +3,18 @@ package com.dongtronic.diabot.data.redis
 import com.dongtronic.diabot.data.ConversionDTO
 import java.time.ZonedDateTime
 
-class NightscoutDTO {
-    var glucose: ConversionDTO? = null
-    var delta: ConversionDTO? = null
-    var dateTime: ZonedDateTime? = null
-    var deltaIsNegative: Boolean = false
-    var low: Int = 0
-    var bottom: Int = 0
-    var top: Int = 0
-    var high: Int = 0
-    var trend: Int = 0
-    var iob: Float = 0.0F
-    var cob: Int = 0
-    var units: String = ""
+data class NightscoutDTO (
+    var glucose: ConversionDTO? = null,
+    var delta: ConversionDTO? = null,
+    var dateTime: ZonedDateTime? = null,
+    var deltaIsNegative: Boolean = false,
+    var low: Int = 0,
+    var bottom: Int = 0,
+    var top: Int = 0,
+    var high: Int = 0,
+    var trend: Int = 0,
+    var iob: Float = 0.0F,
+    var cob: Int = 0,
+    var units: String = "",
     var title: String = "Nightscout"
-    set (value) {
-        if (value != null) {
-            field = value
-        }
-    }
-
-}
+)
