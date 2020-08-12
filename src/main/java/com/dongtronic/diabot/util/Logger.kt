e27961507e160217356ef2b2062c1bd0169b28dc
p5nbTgip0r <cb3f02c5@opayq.com>
2020-08-12 01:45:17 -0700

    Add documentation for `logger` function

--- src/main/java/com/dongtronic/diabot/util/Logger.kt
+++ src/main/java/com/dongtronic/diabot/util/Logger.kt
@@ -3,8 +3,14 @@ package com.dongtronic.diabot.util
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+/**
+ * Creates a SLF4J logger named with the class which it is called from.
+ *
+ * @return A SLF4J [Logger] instance named with the calling class.
+ */
 inline fun <reified T> T.logger(): Logger {
     if (T::class.isCompanion) {
+        // grab the parent class if this class is a companion object
         return LoggerFactory.getLogger(T::class.java.enclosingClass)
     }
 
