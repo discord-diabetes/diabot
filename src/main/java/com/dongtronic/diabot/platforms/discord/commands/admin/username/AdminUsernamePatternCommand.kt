c0e21c121208a5e1e9592ea434e850e5a0711940
p5nbTgip0r <cb3f02c5@opayq.com>
2020-08-11 19:54:14 -0700

    Use MongoDB for username enforcement

--- src/main/java/com/dongtronic/diabot/platforms/discord/commands/admin/username/AdminUsernamePatternCommand.kt
+++ src/main/java/com/dongtronic/diabot/platforms/discord/commands/admin/username/AdminUsernamePatternCommand.kt
@@ -1,6 +1,6 @@
 package com.dongtronic.diabot.platforms.discord.commands.admin.username
 
-import com.dongtronic.diabot.data.redis.AdminDAO
+import com.dongtronic.diabot.data.mongodb.NameRuleDAO
 import com.dongtronic.diabot.platforms.discord.commands.DiscordCommand
 import com.dongtronic.diabot.util.logger
 import com.jagrosh.jdautilities.command.Command
@@ -37,14 +37,24 @@ class AdminUsernamePatternCommand(category: Command.Category, parent: Command?) 
         val patternString = args.joinToString(" ")
         val pattern = patternString.toRegex()
 
-        AdminDAO.getInstance().setUsernamePattern(event.guild.id, pattern.pattern)
-
-        event.reply("Set username enforcement pattern to `$patternString`")
+        NameRuleDAO.instance.setPattern(event.guild.id, pattern.pattern).subscribe({
+            event.reply("Set username enforcement pattern to `$patternString`")
+        }, {
+            logger.warn("Could not set username enforcement pattern for guild ${event.guild.id}", it)
+            event.replyError("Could not set username enforcement pattern for ${event.guild.name}")
+        })
     }
 
     private fun getPattern(event: CommandEvent) {
-        val pattern = AdminDAO.getInstance().getUsernamePattern(event.guild.id)
-
-        event.reply("Current username pattern: `$pattern`")
+        NameRuleDAO.instance.getGuild(event.guild.id).subscribe({
+            event.reply("Current username pattern: `${it.pattern}`")
+        }, {
+            if (it is NoSuchElementException) {
+                event.reply("There is no username enforcement pattern set")
+            } else {
+                logger.warn("Could not get username enforcement pattern for guild ${event.guild.id}", it)
+                event.replyError("Could not get username enforcement pattern for ${event.guild.name}")
+            }
+        })
     }
 }
