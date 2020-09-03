491d75cb987b7ed06e2ee6a72d70cc4b3283088b
p5nbTgip0r <cb3f02c5@opayq.com>
2020-09-02 23:27:21 -0700

    Switch to OkHttp for awyisser

--- src/main/java/com/dongtronic/diabot/logic/fun/Awyisser.kt
+++ src/main/java/com/dongtronic/diabot/logic/fun/Awyisser.kt
@@ -1,35 +1,43 @@
 package com.dongtronic.diabot.logic.`fun`
 
 import com.dongtronic.diabot.exceptions.RequestStatusException
-import com.google.gson.JsonParser
-import org.apache.http.client.methods.RequestBuilder
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.util.EntityUtils
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import reactor.core.publisher.Mono
 
 object Awyisser {
     const val url = "http://awyisser.com/api/generator"
+    private val httpClient = OkHttpClient()
 
     /**
      * Generate Awyiss comic with given input
      * @return URL to the generated image
      */
-    fun generate(input: String): String {
-        val client = HttpClients.createDefault()
-        val request = RequestBuilder.post()
+    fun generate(input: String): Mono<String> {
+        val requestBody = FormBody.Builder()
+                .add("phrase", input)
+                .build()
+        val request = Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build()
 
-        //Add any parameter if u want to send it with Post req.
-        request.addParameter("phrase", input)
-        request.setUri(url)
+        return Mono.fromCallable {
+            httpClient.newCall(request).execute()
+        }.map {
+            it.use { response ->
+                if (response.isSuccessful) {
+                    val body = response.body!!.string()
+                    val json = jacksonObjectMapper().readTree(body)
 
-        val response = client.execute(request.build())
-
-        if (response.statusLine.statusCode == -1) {
-            throw RequestStatusException(-1)
+                    json.get("link")?.textValue()
+                            ?: throw Exception("Could not find awyiss image link")
+                } else {
+                    throw RequestStatusException(it.code)
+                }
+            }
         }
-
-        val json = EntityUtils.toString(response.entity)
-
-        val jsonObject = JsonParser().parse(json).asJsonObject
-        return jsonObject.get("link").asString
     }
 }
