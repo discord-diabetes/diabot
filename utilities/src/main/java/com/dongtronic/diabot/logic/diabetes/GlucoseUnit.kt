84c730af43b730c6bb0be030c8cab042bfbeec3b
p5nbTgip0r <cb3f02c5@opayq.com>
2020-10-19 21:18:08 -0700

    Add lookup functionality to `GlucoseUnit` and add name for `mmol` to be matched. Nightscout allows `mmol/l` and `mmol` to be set as the display units. To support both of these with lookups, and to allow for exact matching (rather than just doing `.startsWith("mmol")`), I opted for a list of names which identify an individual GlucoseUnit.

--- utilities/src/main/java/com/dongtronic/diabot/logic/diabetes/GlucoseUnit.kt
+++ utilities/src/main/java/com/dongtronic/diabot/logic/diabetes/GlucoseUnit.kt
@@ -1,5 +1,35 @@
 package com.dongtronic.diabot.logic.diabetes
 
-enum class GlucoseUnit(internal var unit: String) {
-    MMOL("mmol/L"), MGDL("mg/dL"), AMBIGUOUS("Ambiguous")
+enum class GlucoseUnit(vararg units: String) {
+    // nightscout uses "mmol/l" and "mmol"
+    // https://github.com/nightscout/cgm-remote-monitor#required
+    MMOL("mmol/L", "mmol"),
+    MGDL("mg/dL"),
+    // this unit shouldn't be identified by name
+    AMBIGUOUS("Ambiguous");
+
+    /**
+     * A list of names which identify this particular [GlucoseUnit]
+     */
+    val units: List<String> = units.toList()
+
+    companion object {
+        /**
+         * Attempts to find a [GlucoseUnit] which matches the unit name given.
+         * This function does not match [GlucoseUnit.AMBIGUOUS].
+         *
+         * @param unit The name of the unit to look up
+         * @return [GlucoseUnit] if a unit was matched, null if no matches were found
+         */
+        fun byName(unit: String): GlucoseUnit? {
+            return values().firstOrNull { glucoseUnit ->
+                if (glucoseUnit == AMBIGUOUS)
+                    return@firstOrNull false
+
+                glucoseUnit.units.any { name ->
+                    name.equals(unit, ignoreCase = true)
+                }
+            }
+        }
+    }
 }
