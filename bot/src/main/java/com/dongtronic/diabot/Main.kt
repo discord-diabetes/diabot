a402426279bced3a7ad5c8fae6c8a668713bc46c
p5nbTgip0r <cb3f02c5@opayq.com>
2021-02-28 21:40:42 -0800

    Clean up main class

--- bot/src/main/java/com/dongtronic/diabot/Main.kt
+++ bot/src/main/java/com/dongtronic/diabot/Main.kt
@@ -32,9 +32,6 @@ import com.dongtronic.diabot.platforms.discord.listeners.*
 import com.dongtronic.diabot.util.logger
 import com.github.ygimenez.method.Pages
 import com.github.ygimenez.model.PaginatorBuilder
-import com.jagrosh.jdautilities.command.Command.Category
-import com.jagrosh.jdautilities.command.CommandClientBuilder
-import com.jagrosh.jdautilities.commons.waiter.EventWaiter
 import io.leangen.geantyref.TypeToken
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Message
@@ -43,7 +40,6 @@ import net.dv8tion.jda.api.requests.GatewayIntent
 import net.dv8tion.jda.api.utils.ChunkingFilter
 import net.dv8tion.jda.api.utils.MemberCachePolicy
 import net.dv8tion.jda.api.utils.cache.CacheFlag
-import reactor.core.scheduler.Schedulers
 import java.util.*
 import java.util.function.BiFunction
 import java.util.function.Function
@@ -62,89 +58,7 @@ object Main {
         // Migrate data from Redis to MongoDB
         MigrationManager().migrateIfNecessary()
 
-        val token = System.getenv("DIABOTTOKEN") // token on dokku
-
-        // create command categories
-        val adminCategory = Category("Admin")
-        val bgCategory = Category("BG conversions")
-        val a1cCategory = Category("A1c estimations")
-        val funCategory = Category("Fun")
-        val utilitiesCategory = Category("Utilities")
-        val infoCategory = Category("Informative")
-
-        // define an eventwaiter, dont forget to add this to the JDABuilder!
-        val waiter = EventWaiter()
-
-        // define a command client
-        val client = CommandClientBuilder()
-
-        // The default is "Type !!help" (or whatver prefix you set)
-        client.useDefaultGame()
-        client.useHelpBuilder(true)
-
-        // sets emojis used throughout the bot on successes, warnings, and failures
-        client.setEmojis("\uD83D\uDC4C", "\uD83D\uDE2E", "\uD83D\uDE22")
-
-
-        // sets the bot prefix
-        if (System.getenv("DIABOT_DEBUG") != null) {
-            client.setPrefix("dl ")
-        } else {
-            client.setPrefix("diabot ")
-        }
-
-        client.setOwnerId(ownerIds.first())
-
-        client.setCoOwnerIds(*ownerIds.drop(1).plus(coOwnerIds).toTypedArray())
-
-        client.setServerInvite("https://discord.gg/diabetes")
-
-        // adds commands
-        client.addCommands(
-                // command to show information about the bot
-//                AboutCommand(java.awt.Color(0, 0, 255), "a diabetes bot",
-//                        arrayOf("Converting between mmol/L and mg/dL", "Performing A1c estimations", "Showing Nightscout information"),
-//                        Permission.MESSAGE_ADD_REACTION, Permission.MESSAGE_EMBED_LINKS, Permission.MANAGE_ROLES, Permission.MESSAGE_EXT_EMOJI, Permission.MESSAGE_HISTORY, Permission.MESSAGE_MANAGE, Permission.MESSAGE_READ, Permission.MESSAGE_WRITE, Permission.NICKNAME_MANAGE),
-
-
-                // A1c
-//                EstimationCommand(a1cCategory)
-
-                // BG
-//                ConvertCommand(bgCategory),
-//                NightscoutCommand(bgCategory),
-//                NightscoutGraphCommand(bgCategory),
-
-                // Utility
-//                PingCommand(utilitiesCategory),
-//                RewardsCommand(utilitiesCategory),
-//                GithubCommand(utilitiesCategory),
-//                DisclaimerCommand(utilitiesCategory),
-//                NutritionCommand(utilitiesCategory),
-
-                // Info
-//                InfoCommand(infoCategory),
-//                SupportCommand(infoCategory),
-
-                // Fun
-//                ExcuseCommand(funCategory),
-//                AwyissCommand(funCategory),
-//                DiacastCommand(funCategory),
-//                OwnerCommand(funCategory),
-//                QuoteCommand(funCategory)
-
-                // Admin
-//                AdminCommand(adminCategory),
-//                ShutdownCommand(adminCategory),
-//                NightscoutAdminCommand(adminCategory),
-//                RolesCommand(adminCategory)
-        )
-
-
-        // Custom help handler
-        client.setHelpConsumer(HelpListener())
-
-        val builtClient = client.build()
+        val token = System.getenv("DIABOTTOKEN")
 
         val cmdUpdateHandler = JDACommandUpdateHandler(100)
         val jda = JDABuilder.createLight(token)
@@ -158,16 +72,7 @@ object Main {
                 .setMemberCachePolicy(MemberCachePolicy.ALL) // Cache all members regardless of their online state
                 .setChunkingFilter(ChunkingFilter.ALL) // Cache all guilds on initialisation
                 .disableCache(EnumSet.allOf(CacheFlag::class.java)) // We don't need any JDA cache services
-                .addEventListeners(
-                        waiter,
-                        builtClient,
-                        ConversionListener(cmdUpdateHandler),
-                        RewardListener(),
-                        UsernameEnforcementListener(),
-                        OhNoListener(),
-//                        QuoteListener(builtClient),
-                        cmdUpdateHandler
-                ).build()
+                .build()
 
         // Pagination
         val paginator = PaginatorBuilder.createPaginator()
@@ -176,8 +81,9 @@ object Main {
                 .build()
         Pages.activate(paginator)
 
-        // this will be changed later once the jda-utilities command framework is removed
-        val prefix = "!"
+        // sets the bot prefix
+        val prefix = if (System.getenv("DIABOT_DEBUG") != null) "dl " else "diabot "
+
         val permissionRegistry = PermissionRegistry()
         val commandManager: JDA4CommandManager<JDACommandUser> = JDA4CommandManager(
                 jda,
@@ -197,7 +103,7 @@ object Main {
                 }
         )
 
-        // override default user parser. as of cloud 1.5.0 the default isolation is GLOBAL, but GUILD is usually a safer bet.
+        // override default user parser. as of cloud 1.5.0 the default isolation is GLOBAL, but GUILD is usually safer
         commandManager.parserRegistry.registerParserSupplier(TypeToken.get(User::class.java)) {
             UserParser<JDACommandUser>(UserArgument.ParserMode.values().toSet(), Isolation.GUILD)
         }
@@ -218,7 +124,6 @@ object Main {
                 prefix,
                 { sender: JDACommandUser, message: Message ->
                     sender.reply(message, ReplyType.NONE)
-                            .subscribeOn(Schedulers.boundedElastic())
                 },
                 { sender: JDACommandUser, user: User ->
                     user.id == sender.getAuthorUniqueId()
@@ -227,6 +132,7 @@ object Main {
 
         DiabotParser(commandManager, JDACommandUser::class.java)
                 .addAutoPermissionSupport()
+                .addDiscordPermissionSupport()
                 .addCategorySupport()
                 .addExampleSupport()
                 .addOwnersOnlySupport(*allOwnerIds) {}
@@ -243,7 +149,7 @@ object Main {
                     it.commandContext.sender.replyErrorS(message)
                 }
                 .addCooldownSupport(
-                        {
+                        idMapper = {
                             val guildId = if (it.event.isFromGuild) it.event.guild.id else ""
                             CooldownIds(
                                     userId = it.getAuthorUniqueId(),
@@ -252,13 +158,12 @@ object Main {
                                     shardId = it.event.jda.shardInfo.shardId.toString()
                             )
                         },
-                        { millisRemaining: Long, _: CooldownMeta, context: CommandPostprocessingContext<JDACommandUser> ->
+                        notifier = { millisRemaining: Long, _: CooldownMeta, context: CommandPostprocessingContext<JDACommandUser> ->
                             val secsLeft = (millisRemaining / 1000).toInt()
                             val s = if (secsLeft == 1) "" else "s"
                             context.commandContext.sender.replyErrorS("This command is currently in cooldown for $secsLeft more second$s")
                         }
                 )
-                .addDiscordPermissionSupport()
                 .parse(arrayOf(
                         // A1c
                         EstimationCommand(),
@@ -295,7 +200,16 @@ object Main {
                         RolesCommand()
                 ))
 
-        jda.addEventListener(QuoteListener(commandManager, cmdUpdateHandler))
+        // add event listeners
+        jda.addEventListener(
+                ConversionListener(cmdUpdateHandler),
+                RewardListener(),
+                UsernameEnforcementListener(),
+                OhNoListener(),
+                QuoteListener(commandManager, cmdUpdateHandler),
+                cmdUpdateHandler
+        )
+
         commandManager.commandHelpHandler.allCommands.forEach {
             // debug code
             logger.debug("Syntax: ${it.syntaxString}; Desc: ${it.description}")
