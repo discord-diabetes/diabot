b27906f567a2f8273724dbb6b3b913ff258d0e65
p5nbTgip0r <cb3f02c5@opayq.com>
2021-12-13 20:55:52 -0800

    refactor(conversion): Remove unnecessary nullable on conversion result

--- bot/src/main/java/com/dongtronic/diabot/platforms/discord/listeners/ConversionListener.kt
+++ bot/src/main/java/com/dongtronic/diabot/platforms/discord/listeners/ConversionListener.kt
@@ -60,15 +60,15 @@ class ConversionListener : ListenerAdapter() {
                 numberString = numberString.replace(',', '.')
             }
 
-            val result: ConversionDTO? = if (originalUnitString.length > 1) {
+            val result: ConversionDTO = if (originalUnitString.length > 1) {
                 BloodGlucoseConverter.convert(numberString, originalUnitString)
             } else {
                 BloodGlucoseConverter.convert(numberString, null)
             }
 
             finalMessage += when {
-                result!!.inputUnit === GlucoseUnit.MMOL -> String.format("%s mmol/L is %s mg/dL", result!!.mmol, result.mgdl)
-                result!!.inputUnit === GlucoseUnit.MGDL -> String.format("%s mg/dL is %s mmol/L", result!!.mgdl, result.mmol)
+                result.inputUnit === GlucoseUnit.MMOL -> String.format("%s mmol/L is %s mg/dL", result.mmol, result.mgdl)
+                result.inputUnit === GlucoseUnit.MGDL -> String.format("%s mg/dL is %s mmol/L", result.mgdl, result.mmol)
                 else -> {
                     val reply = arrayOf(
                             "*I'm not sure if you gave me mmol/L or mg/dL, so I'll give you both.*",
@@ -76,7 +76,7 @@ class ConversionListener : ListenerAdapter() {
                             "%s mmol/L is **%s mg/dL**").joinToString(
                             "%n")
 
-                    String.format(reply, numberString, result!!.mmol, numberString, result.mgdl)
+                    String.format(reply, numberString, result.mmol, numberString, result.mgdl)
                 }
             }
 
