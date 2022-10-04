a9e68daf0461afe7f22541020a978b46232cec59
Cas Eliens <github@cascer1.space>
2022-10-04 15:26:17 +0200

    Use new rest awyisser API
    #173

--- bot/src/main/java/com/dongtronic/diabot/logic/fun/Awyisser.kt
+++ bot/src/main/java/com/dongtronic/diabot/logic/fun/Awyisser.kt
@@ -1,27 +1,31 @@
 package com.dongtronic.diabot.logic.`fun`
 
+import com.dongtronic.diabot.data.`fun`.AwyisserRequestDTO
 import com.dongtronic.diabot.exceptions.RequestStatusException
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import okhttp3.FormBody
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import reactor.core.publisher.Mono
 
 object Awyisser {
-    const val url = "http://awyisser.com/api/generator"
+    const val url = "https://awyisser.com/api/generator"
     private val httpClient = OkHttpClient()
+    private val mapper = jacksonObjectMapper()
 
     /**
      * Generate Awyiss comic with given input
      * @return URL to the generated image
      */
     fun generate(input: String): Mono<String> {
-        val requestBody = FormBody.Builder()
-                .add("phrase", input)
-                .build()
+        val requestObject = AwyisserRequestDTO(value = input)
+        val jsonRequest = mapper.writeValueAsString(requestObject)
+        val mediaType = "application/json".toMediaType()
+
         val request = Request.Builder()
                 .url(url)
-                .post(requestBody)
+                .post(jsonRequest.toRequestBody(mediaType))
                 .build()
 
         return Mono.fromCallable {
@@ -32,7 +36,7 @@ object Awyisser {
                     val body = response.body!!.string()
                     val json = jacksonObjectMapper().readTree(body)
 
-                    json.get("link")?.textValue()
+                    json.get("image")?.textValue()
                             ?: throw Exception("Could not find awyiss image link")
                 } else {
                     throw RequestStatusException(it.code)
