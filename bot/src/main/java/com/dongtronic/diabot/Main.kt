f65fabd5ecdd5d7856ee6eb274105a1b3fa69b12
Cas Eliens <github@cascer1.space>
2022-10-06 13:07:49 +0200

    Delete old awyiss command, update diacast feed url

--- bot/src/main/java/com/dongtronic/diabot/Main.kt
+++ bot/src/main/java/com/dongtronic/diabot/Main.kt
@@ -101,7 +101,6 @@ object Main {
 
                 // Fun
                 ExcuseCommand(funCategory),
-                AwyissCommand(funCategory),
                 DiacastCommand(funCategory),
                 OwnerCommand(funCategory),
                 QuoteCommand(funCategory),
