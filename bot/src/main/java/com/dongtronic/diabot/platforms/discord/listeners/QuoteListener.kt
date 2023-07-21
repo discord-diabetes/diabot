6750bc4f22b1e22c564b3158692c339f5b096cdf
p5nbTgip0r <cb3f02c5@opayq.com>
2023-07-21 00:01:37 -0700

    refactor(quote): swap quote listener to async
    Reactive code is kind of messy and hard to follow, and this function was already a `suspend` func, so I decided to swap it over to async calls.

--- bot/src/main/java/com/dongtronic/diabot/platforms/discord/listeners/QuoteListener.kt
+++ bot/src/main/java/com/dongtronic/diabot/platforms/discord/listeners/QuoteListener.kt
@@ -9,17 +9,14 @@ import com.jagrosh.jdautilities.command.CommandClient
 import com.jagrosh.jdautilities.command.CommandEvent
 import dev.minn.jda.ktx.coroutines.await
 import dev.minn.jda.ktx.events.CoroutineEventListener
-import net.dv8tion.jda.api.entities.Message
+import kotlinx.coroutines.reactive.awaitFirstOrNull
 import net.dv8tion.jda.api.entities.MessageType
 import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.entities.emoji.UnicodeEmoji
 import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent
-import net.dv8tion.jda.api.requests.restaction.MessageCreateAction
 import org.litote.kmongo.eq
-import reactor.kotlin.core.publisher.toMono
-import java.util.function.Consumer
 
 class QuoteListener(private val client: CommandClient) : CoroutineEventListener {
     private val quoteCommand: QuoteCommand = client.commands.filterIsInstance(QuoteCommand::class.java).first()
@@ -35,60 +32,58 @@ class QuoteListener(private val client: CommandClient) : CoroutineEventListener 
         }
     }
 
+    @Suppress("CyclomaticComplexMethod", "ReturnCount")
     private suspend fun onMessageReactionAdd(event: MessageReactionAddEvent) {
-        if (event.retrieveUser().await().isBot) return
         if (!event.isFromGuild) return
-        if (event.reaction.emoji !is UnicodeEmoji) return
-        if (event.reaction.emoji != speechEmoji) return
+        if (event.reaction.emoji !is UnicodeEmoji || event.reaction.emoji != speechEmoji) return
+
+        val reactor = event.retrieveMember().await()
+
+        if (reactor.user.isBot) return
         if (!QuoteDAO.checkRestrictions(event.guildChannel)) return
 
         val message = event.retrieveMessage().await()
-        val author = event.retrieveMember().await()
-        val guild = event.guild
 
+        if (message.author.isBot) return
+        if (message.type != MessageType.DEFAULT && message.type != MessageType.INLINE_REPLY) return
         // Ignore event if the bot already created a quote from this message
         if (message.reactions.any { it.emoji == speechEmoji && it.isSelf }) return
 
-        /**
-         * Replies to the channel only if the reacting user has permission to send messages
-         */
-        val reply: (() -> MessageCreateAction) -> Unit = { msg ->
-            if (event.guildChannel.canTalk(author)) {
-                msg().queue()
-            }
+        // Only add quote if there are no quotes with the message ID
+        try {
+            val quote = QuoteDAO.getInstance().getQuotes(event.guild.id, QuoteDTO::messageId eq event.messageId).awaitFirstOrNull()
+            if (quote != null) return
+        } catch (_: NoSuchElementException) {
         }
 
-        val quoteMessage = Consumer<Message> { msg ->
+        val quote = try {
             QuoteDAO.getInstance().addQuote(
                     QuoteDTO(
-                            guildId = guild.id,
+                            guildId = event.guild.id,
                             channelId = event.guildChannel.id,
-                            author = msg.author.name,
-                            authorId = msg.author.id,
-                            quoterId = event.userId,
-                            message = msg.contentRaw,
-                            messageId = msg.id
+                            author = message.author.name,
+                            authorId = message.author.id,
+                            quoterId = reactor.id,
+                            message = message.contentRaw,
+                            messageId = message.id
                     )
-            ).subscribe({
-                msg.addReaction(speechEmoji).queue()
-                reply { event.guildChannel.sendMessage(QuoteAddCommand.createAddedMessage(author.asMention, it.quoteId!!, msg.jumpUrl)) }
-            }, {
-                reply { event.guildChannel.sendMessage("Could not create quote for message: ${msg.id}") }
-            })
+            ).awaitFirstOrNull()
+        } catch (_: Exception) {
+            null
         }
 
-        QuoteDAO.getInstance()
-                // search for any quotes with this message ID
-                .getQuotes(guild.id, QuoteDTO::messageId eq event.messageId)
-                .toMono()
-                .subscribe({ /*ignored*/ }, { error ->
-                    // only add quote if there are no quotes matching the reacted message ID
-                    if (error is NoSuchElementException) {
-                        message.toMono()
-                                .filter { (it.type == MessageType.DEFAULT || it.type == MessageType.INLINE_REPLY) && !it.author.isBot }
-                                .subscribe(quoteMessage)
-                    }
-                })
+        if (quote != null) {
+            message.addReaction(speechEmoji).await()
+        }
+
+        // Don't reply if the reacting user doesn't have permission to send messages in the channel
+        if (!event.guildChannel.canTalk(reactor)) return
+
+        if (quote != null) {
+            event.guildChannel.sendMessage(QuoteAddCommand.createAddedMessage(reactor.asMention, quote.quoteId!!, message.jumpUrl)).await()
+        } else {
+            event.guildChannel.sendMessage("Could not create quote for message: ${message.id}").await()
+        }
     }
 
     private fun onMessageReceived(event: MessageReceivedEvent) {
