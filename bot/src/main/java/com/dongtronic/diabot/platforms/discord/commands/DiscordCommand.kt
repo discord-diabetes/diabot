6c3149f130b8d59dee2694d7f450de6e18d3f494
p5nbTgip0r <cb3f02c5@opayq.com>
2023-10-16 16:13:18 -0700

    feat: add coroutine base for prefix/text commands

--- bot/src/main/java/com/dongtronic/diabot/platforms/discord/commands/DiscordCommand.kt
+++ bot/src/main/java/com/dongtronic/diabot/platforms/discord/commands/DiscordCommand.kt
@@ -1,6 +1,10 @@
 package com.dongtronic.diabot.platforms.discord.commands
 
 import com.jagrosh.jdautilities.command.Command
+import com.jagrosh.jdautilities.command.CommandEvent
+import dev.minn.jda.ktx.events.CoroutineEventManager
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 abstract class DiscordCommand(category: Category, parent: Command?) : Command() {
     var examples = arrayOfNulls<String>(0)
@@ -12,6 +16,25 @@ abstract class DiscordCommand(category: Category, parent: Command?) : Command() 
         this.category = category
         this.parent = parent
     }
+
+    override fun execute(event: CommandEvent) {
+        // The `.injectKTX()` line in the main class changes the event manager to a CoroutineEventManager
+        val manager = event.jda.eventManager as? CoroutineEventManager
+        if (manager == null) {
+            // fallback to blocking if the event manager isn't a CoroutineEventManager for some reason
+            runBlocking {
+                launch {
+                    executeSuspend(event)
+                }
+            }
+        } else {
+            manager.launch {
+                executeSuspend(event)
+            }
+        }
+    }
+
+    open suspend fun executeSuspend(event: CommandEvent) {}
 
     override fun toString(): String {
         return if (this.parent != null) {
